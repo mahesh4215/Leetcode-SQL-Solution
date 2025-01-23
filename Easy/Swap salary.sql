bd26a093face078950cdda1e959820bd8b1fe8ab
Mahesh Tiwari <54504621+mahesh4215@users.noreply.github.com>
2025-01-23 11:51:56 +0530

    Create Swap salary.sql

---
+++ Easy/Swap salary.sql
@@ -0,0 +1,30 @@
+Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.
+
+Example 1:
+
+Input: 
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+Output: 
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
+
+solution : Using update and Case statement
+
+update salary
+  SET sex = case when sex = 'm' then 'f' else 'm' end ;
