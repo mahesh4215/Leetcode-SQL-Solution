eba46b7b37bdb7ea22e637b2c9384e61c32c7057
Mahesh Tiwari <54504621+mahesh4215@users.noreply.github.com>
2025-01-27 11:08:31 +0530

    Create Queries Quality and Percentage.sql

---
+++ Easy/Queries Quality and Percentage.sql
@@ -0,0 +1,43 @@
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Input: 
+Queries table:
++------------+-------------------+----------+--------+
+| query_name | result            | position | rating |
++------------+-------------------+----------+--------+
+| Dog        | Golden Retriever  | 1        | 5      |
+| Dog        | German Shepherd   | 2        | 5      |
+| Dog        | Mule              | 200      | 1      |
+| Cat        | Shirazi           | 5        | 2      |
+| Cat        | Siamese           | 3        | 3      |
+| Cat        | Sphynx            | 7        | 4      |
++------------+-------------------+----------+--------+
+Output: 
++------------+---------+-----------------------+
+| query_name | quality | poor_query_percentage |
++------------+---------+-----------------------+
+| Dog        | 2.50    | 33.33                 |
+| Cat        | 0.66    | 33.33                 |
++------------+---------+-----------------------+
+
+Solution: using round and case statement.
+SELECT
+    query_name,
+    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(
+        SUM(
+            CASE
+                WHEN rating < 3 THEN 1
+                ELSE 0
+            END
+        ) * 100.0 / COUNT(query_name)
+    , 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
+Order by quality desc;
